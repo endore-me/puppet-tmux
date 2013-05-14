ad821ab12e2f2cf8be32cfebac956b8d2f62145e
Jens Fahnenbruck <jigfox@me.com>
2013-05-14 11:32:01 +0200

    adds first spec for tmux module

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'puppet', '2.7.21'
+gem 'facter', '1.6.17'
+gem 'rake', '10.0.4'
+gem 'puppetlabs_spec_helper', '0.4.1'
---
+++ spec/classes/tmux_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'tmux', type: 'class' do
+  context "On a Debian OS with no package name specified" do
+    let :facts do
+      { osfamily: 'Debian' }
+    end
+    it { should contain_package('tmux') }
+  end
+end
